afbf4dd46be4545f6f02e9c8468ef88bab9e3498
Ashutosh lakshakar <lakshakarashutosh@gmail.com>
2024-03-06 18:27:13 +0530

    feat: done

--- src/app/Components/invoicecreate/Invoice.tsx
+++ src/app/Components/invoicecreate/Invoice.tsx
@@ -68,7 +68,7 @@ const Invoice = () => {
 
             {/* -------------- */}
 
-            <div className="SubTotal sm:px-2 py-6  w-1/2">
+            <div className="SubTotal sm:px-2 py-4  w-1/2">
               <div className="grid grid-cols-2 gap-2 sm:gap-4">
                 <div className="text-[.5rem] md:text-[.8rem]">
                   <p className="text-end font-semibold py-2">Subtotal:</p>
